ead511a48151a2bfb31aea1254008aa5fe5cad02
alian20068 <newjoin@foxmail.com>
2019-02-13 21:47:09 +0800

    读写SD卡

--- SaveFile6/app/src/main/java/com/newjoin/savefile6/MainActivity.java
+++ SaveFile6/app/src/main/java/com/newjoin/savefile6/MainActivity.java
@@ -3,6 +3,7 @@ package com.newjoin.savefile6;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -18,6 +19,8 @@ import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,32 +28,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ReadSd(View view) {
-        //android.permission.READ_EXTERNAL_STORAGE 写外部存储的权限
-        //4.0以前版本，读不需要权限
-        if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
-            try {
-                File file = new File(Environment.getExternalStorageDirectory() + "mysd.txt");
-                FileInputStream fis = new FileInputStream(file);
-                BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-                String str = br.readLine();
-                Toast.makeText(this, "读取成功" + str, Toast.LENGTH_SHORT).show();
-            } catch (Exception e) {
-                e.printStackTrace();
-                Toast.makeText(this, "读取失败", Toast.LENGTH_SHORT).show();
-            }
-        } else {
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
             Toast.makeText(this, "SD卡非挂载状态", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //android.permission.READ_EXTERNAL_STORAGE 读外部存储的权限
+        //4.0以前版本，读不需要权限
+        try {
+            File file = new File(Environment.getExternalStorageDirectory() + "/mysd.txt");
+            FileInputStream fis = new FileInputStream(file);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+            String str = br.readLine();
+            Toast.makeText(this, "读取成功" + str, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "读取失败", Toast.LENGTH_SHORT).show();
         }
     }
 
     public void WriteSd(View view) {
-        if (Environment.getExternalStorageState() != Environment.MEDIA_MOUNTED) {
+        Log.d(TAG, "SD卡状态" + Environment.getExternalStorageState());
+        Log.d(TAG, "枚举状态" + Environment.MEDIA_MOUNTED);
+
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
             Toast.makeText(this, "SD卡非挂载状态", Toast.LENGTH_SHORT).show();
             return;
         }
         //android.permission.WRITE_EXTERNAL_STORAGE 写外部存储的权限
         try {
-            File file = new File(Environment.getExternalStorageDirectory() + "mysd.txt");
+            File file = new File(Environment.getExternalStorageDirectory() + "/mysd.txt");
             FileOutputStream fos = new FileOutputStream(file);
             fos.write("default".getBytes());
             fos.close();
